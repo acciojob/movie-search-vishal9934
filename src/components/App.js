81673861f292fdb2f5b9d5000101bad21152e28d
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-06-15 10:50:32 +0530

    submitting code-Thu Jun 15 2023 10:50:31 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-
 import React from "react";
 import './../styles/App.css';
+import SearchBar from "./Searchbar";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <SearchBar />
     </div>
   )
 }
